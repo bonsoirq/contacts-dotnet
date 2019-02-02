bcd541a135bc77813f975b0d74689d47c50db601
Jarosław Bąk <jaroslaw.bak24@gmail.com>
2019-02-02 17:27:10 +0100

    Add file actions

--- ContactsDotNet/ContactsDotNet/MainWindow.xaml.cs
+++ ContactsDotNet/ContactsDotNet/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +30,34 @@ namespace ContactsDotNet
         private void About_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Kontakty ⓒ 2019 Jarosław Bąk & Maksymilian Galas", "O aplikacji");
+        }
+
+        private void NewFile_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new SaveFileDialog();
+            fileDialog.AddExtension = true;
+            fileDialog.FileName = "kontakty";
+            fileDialog.DefaultExt = "cnt";
+            if (fileDialog.ShowDialog(this) == true)
+            {
+                File.Create(fileDialog.FileName);
+            }
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog(this) == true)
+            {
+                var stream = new FileStream(fileDialog.FileName, FileMode.Open);
+                var reader = new StreamReader(stream);
+                var contactsData = new List<string>();
+                while (reader.ReadLine() != null)
+                {
+                    contactsData.Add(reader.ReadLine());
+                }
+                reader.Close();
+            }
         }
     }
 }
